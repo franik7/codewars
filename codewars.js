c3fe1534b64d95eb395f36d7533124d169e5fad8
Frants <118231497+franik7@users.noreply.github.com>
2023-04-07 20:24:35 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -238,3 +238,26 @@ function repeatStr(n, s) {
 		    }
 		return arr
 	  }
+
+	
+	 //#13: Number object and its properties
+
+	  function whatNumberIsIt(n){
+		//coding here
+		if (n === Number.MAX_VALUE) {
+		  return("Input number is Number.MAX_VALUE")
+		} else if (n === Number.MIN_VALUE) {
+		  return("Input number is Number.MIN_VALUE")
+		} else if (n === Number.NEGATIVE_INFINITY) {
+		  return("Input number is Number.NEGATIVE_INFINITY") 
+		}else if (n === Number.POSITIVE_INFINITY) {
+		  return("Input number is Number.POSITIVE_INFINITY")
+	  	}else if (n) {
+		  return(`Input number is ${n}`)
+	  	}else if (n == 0) {
+		  return("Input number is 0")
+	  	}  else if (!n !== NaN) {
+		  return("Input number is Number.NaN")
+	  	}
+		}
+	 	 whatNumberIsIt()

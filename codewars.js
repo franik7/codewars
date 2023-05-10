d38fe8bbcc4db4079b75a5b35813e8f67c4e5861
Frants <118231497+franik7@users.noreply.github.com>
2023-05-09 20:58:44 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -298,3 +298,21 @@ function repeatStr(n, s) {
 			  return("Draw!")
 			} 
 		  };
+	
+	
+	//   Total amount of points
+
+		function points(games) {
+			let total = 0
+		   
+			for(let i = 0; i< games.length; i++){
+			  if(games[i][0] > games[i][2]){
+				total += 3
+			  } else if (games[i][0] < games[i][2]){
+				total += 0
+			  } else {
+				total += 1
+			  }
+			} 
+			return total
+			}

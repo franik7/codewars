57354dc5114771851e238f68f4b17c4cff3395c3
Frants <118231497+franik7@users.noreply.github.com>
2023-05-09 20:26:57 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -287,5 +287,14 @@ function repeatStr(n, s) {
 		  }
 	
 	
-	 //Training JS #15: Methods of Number 
-		  // object--toFixed(), toExponential() and toPrecision()
+	//Rock Paper Scissors!
+
+		  const rps = (p1, p2) => {
+			if(p1 == "scissors" && p2 == "paper" || p1 == "rock" && p2 == "scissors" || p1 == "paper" && p2 == "rock"){
+			  return("Player 1 won!")
+			} else if (p2 == "scissors" && p1 == "paper" || p2 == "rock" && p1 == "scissors" || p2 == "paper" && p1 == "rock"){
+			  return("Player 2 won!")
+			} else if(p1 == "paper" && p2 == "paper" || p1 == "scissors" && p2 == "scissors" || p1 == "rock" && p2 == "rock"){
+			  return("Draw!")
+			} 
+		  };

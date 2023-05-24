d22eab35f0f02dac052cdaade7d46192ed7eb8b9
Frants <118231497+franik7@users.noreply.github.com>
2023-05-24 19:35:42 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -1,6 +1,7 @@
 function repeatStr(n, s) {
 	return s.repeat(n);
 
+	
 	// DESCRIPTION:
 	// In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
 

fbab487482fca226a0f4784817fea0d3d274d0c3
Frants <118231497+franik7@users.noreply.github.com>
2023-07-24 20:53:05 -0400

    Add files via upload

--- codewars.js
+++ codewars.js
@@ -623,3 +623,28 @@
 			  return n*2-2
 			}  
 		  }
+
+		// Switch it Up!
+		function switchItUp(number){
+			if(number == 1){
+			  return "One"
+			} else if(number == 2){
+			  return "Two"
+			}else if(number == 3){
+			  return "Three"
+			}else if(number == 4){
+			  return "Four"
+			}else if(number == 5){
+			  return "Five"
+			}else if(number == 6){
+			  return "Six"
+			}else if(number == 7){
+			  return "Seven"
+			}else if(number == 8){
+			  return "Eight"
+			}else if(number == 9){
+			  return "Nine"
+			}else if(number == 0){
+			  return "Zero"
+			}
+		  }

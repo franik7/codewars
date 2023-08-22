8791e2ff21657c795f1198764b167f48b3d46d96
Frants <118231497+franik7@users.noreply.github.com>
2023-08-22 15:31:18 -0400

    Add files via upload

--- codewars.js
+++ codewars.js
@@ -1341,3 +1341,11 @@ function removeUrlAnchor(url){
 	}  let newStr = newArr.join("")
 	return newStr
 }
+
+
+//MakeUpperCase
+function makeUpperCase(str) {
+	// Code here
+	let up = str.toUpperCase()
+	return up
+  }

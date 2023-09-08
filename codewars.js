e09bc3c51a372f591d766eb8aea07eef7852f6c5
Frants <118231497+franik7@users.noreply.github.com>
2023-09-08 19:25:13 +0200

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -1433,8 +1433,19 @@ for (let i = 0; i < arr.length; i++){
   return str 
 }
 
+	
 //FIXME: Replace all dots	
 var replaceDots = function(str) {
   return (str.replaceAll('.', '-'));
 }
-	
+
+
+//Keep at it until you get it
+function hoopCount (n) {
+   //your code goes here 
+  if(n >= 10){
+    return "Great, now move on to tricks"
+  } else {
+    return "Keep at it until you get it"
+  } 
+}

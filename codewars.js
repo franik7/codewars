50298936bc01f2f7adb9055d9201b3b819acd801
Frants <118231497+franik7@users.noreply.github.com>
2023-10-08 21:32:26 -0400

    Add files via upload

--- codewars.js
+++ codewars.js
@@ -1699,3 +1699,13 @@ function repeatStr(n, s) {
 	return false
 	}
 	}
+
+
+	//Fix the Bugs (Syntax) - My First Kata
+	function myFirstKata(a,b) {
+	if (typeof(a) != "number" || typeof(b) != "number") {
+	return false;
+	} else {
+		return a % b + b % a
+	}
+	}

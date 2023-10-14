9c1464d08386d0ab16404fbf80e0617d4af96c51
Frants <118231497+franik7@users.noreply.github.com>
2023-10-14 19:00:14 -0400

    Add files via upload

--- codewars.js
+++ codewars.js
@@ -1763,3 +1763,17 @@ if(sum + val + happiness > 22){
 	return "Back to your desk, boy."
 }
 } 
+
+
+//Basic Mathematical Operations
+function basicOp(operation, value1, value2){
+ if(operation == "+"){
+  return value1 + value2
+} else if(operation == "-") {
+  return value1 - value2
+} else if(operation == "*"){
+  return value1 * value2  
+} else {
+  return value1/value2
+}
+}

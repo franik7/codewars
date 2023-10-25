9f6a6ffe7e4668d635ad4a88d55842498f2f1779
Frants <118231497+franik7@users.noreply.github.com>
2023-10-25 18:42:00 -0400

    Add files via upload

--- codewars.js
+++ codewars.js
@@ -1822,3 +1822,16 @@ function processData(data){
   newArr = newArr.filter((el) => el !== undefined);
   return newArr;
 }
+
+
+//Sum of positive
+function positiveSum(arr) {
+	let newArr = []
+	for(let i = 0;i<arr.length;i++){
+	  if(arr[i] * 1 > 0){
+		newArr.push(arr[i])
+	  }
+	} 
+  const initialValue = 0;
+  return newArr.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue);
+  }

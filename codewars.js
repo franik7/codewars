6e556b2eb4fde2ed30bf70da5089324dea3934a3
Frants <118231497+franik7@users.noreply.github.com>
2023-11-04 15:38:41 -0400

    Add files via upload

--- codewars.js
+++ codewars.js
@@ -1913,3 +1913,13 @@ function getSum(a, b){
     }}
    return sum
 }
+
+
+//Beginner Series #1 School Paperwork
+function paperwork(n, m) {
+  if(n<0 || m<0){
+    return 0
+  } else {
+    return n * m
+  }
+}

2ee3e8656f34c3804b10deb01899ea093933828c
Frants <118231497+franik7@users.noreply.github.com>
2023-11-26 13:04:23 +0100

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -2089,3 +2089,14 @@ function sum (numbers) {
     return numbers.reduce((accumulator, currentValue) => accumulator + currentValue)
   }
 };
+
+	
+//Find the Missing Number
+function missingNo(nums) {
+  const sorted = nums.sort((a, b) => a - b);
+  for(let i = 0; i<sorted.length;i++){
+    if(sorted[i] + 1 != sorted[i+1]){
+       return sorted[i] + 1
+       }
+  }
+}

93d0fb1f07edebc8eda89de65ead55b1a7835dfe
Frants <118231497+franik7@users.noreply.github.com>
2023-12-06 19:17:54 -0500

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -2168,3 +2168,13 @@ var greet = function(name) {
 
   return "Hello " + upFirstLetter + theRest + "!"
 };
+
+//Super Duper Easy
+function problem(x){
+  //your code here
+   if(typeof(x) == "string"){
+     return "Error"
+   } else {
+     return x * 50 + 6
+   }
+}

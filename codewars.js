24a02e37638e527fd90fc9e81c724ef17e8172fe
Frants <118231497+franik7@users.noreply.github.com>
2023-12-15 17:27:34 -0500

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -2400,4 +2400,17 @@ var findAverage = function (nums) {
     total = total + nums[i]
   } return total/nums.length
 }
-	
+
+
+//Categorize New Member
+function openOrSenior(data){
+  // ...
+  let resultArr = []
+  for(let i = 0 ;i<data.length;i++){
+    if(data[i][0] >= 55 && data[i][1] > 7){
+      resultArr.push("Senior")
+    } else {
+      resultArr.push("Open")
+    }  
+  } return resultArr 
+}

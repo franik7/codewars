4ab01762b54663837be4a8211e018386a7a0c791
Frants <118231497+franik7@users.noreply.github.com>
2024-01-22 18:00:39 -0500

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -2455,3 +2455,16 @@ const reverseSeq = n => {
   }
   return newArr
 };
+
+	
+//Training JS #31: methods of arrayObject---isArray() indexOf() and toString()
+function blackAndWhite(arr){
+  //coding here...
+  if(!Array.isArray(arr)){
+    return ("It's a fake array")
+  } else if (arr.includes(5) && arr.includes(13)){
+    return ("It's a black array") 
+  } else {
+    return ("It's a white array") 
+  } 
+}

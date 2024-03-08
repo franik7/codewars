084104077d5969d899281a5b13df20edf69215d4
Frants <118231497+franik7@users.noreply.github.com>
2024-03-07 21:42:15 -0500

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -2705,3 +2705,36 @@ var filterString = function(value) {
     } 
   }return Number(newArr.join(""))
     }  
+
+
+//Detect Pangram
+function isPangram(string){
+    let str = string.toLowerCase()
+    let arr = str.split("")
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    let alphabetArr = alphabet.split("")
+
+    let newArr = []
+    
+    for(let i = 0; i<arr.length;i++){
+        if(arr[i] === 'a' || arr[i] === 'b' || arr[i] === 'c' || arr[i] === 'd' ||
+        arr[i] === 'e' || arr[i] === 'f' || arr[i] === 'g' || arr[i] === 'h' ||
+        arr[i] === 'i' || arr[i] === 'j' || arr[i] === 'k' || arr[i] === 'l' ||
+        arr[i] === 'm' || arr[i] === 'n' || arr[i] === 'o' || arr[i] === 'p' ||
+        arr[i] === 'q' || arr[i] === 'r' || arr[i] === 's' || arr[i] === 't' ||
+        arr[i] === 'u' || arr[i] === 'v' || arr[i] === 'w' || arr[i] === 'x' ||
+        arr[i] === 'y' || arr[i] === 'z'){
+            newArr.push(arr[i])
+        }
+    }
+
+    let newSet = new Set(newArr)
+    let newArr2 = Array.from(newSet)
+    if(newArr2.length === alphabetArr.length){
+        console.log(true)
+        return true
+    } else {
+        console.log(false)
+        return false
+    }
+}

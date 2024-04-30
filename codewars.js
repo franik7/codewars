e20d58c144ca247784c93d0d9f24340ca9260be1
Frants <118231497+franik7@users.noreply.github.com>
2024-04-29 20:45:52 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -2990,3 +2990,15 @@ function noOdds( values ){
     }
   } return newArr
 }
+
+
+//Find the first non-consecutive number
+function firstNonConsecutive (arr) {
+  console.log(arr)
+  for (let i = 0;i<arr.length - 1;i++){
+    if(arr[i] + 1 != arr[i+1]){
+      console.log(arr[i+1])
+      return arr[i+1]
+    } 
+  }return null
+}

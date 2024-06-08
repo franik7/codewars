f4843ff3e3e2547f81241e845380d86d6dc4d695
Frants <118231497+franik7@users.noreply.github.com>
2024-06-07 23:50:25 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -3338,3 +3338,21 @@ function binToDec(bin) {
   // TODO
   return parseInt(bin, 2)
 }
+
+
+//Grasshopper - Grade book
+function getGrade(s1, s2, s3) {
+    const average = (s1 + s2 + s3) / 3;
+    if (average >= 90 && average <= 100) {
+        return 'A';
+    } else if (average >= 80 && average < 90) {
+        return 'B';
+    } else if (average >= 70 && average < 80) {
+        return 'C';
+    } else if (average >= 60 && average < 70) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+}
+	

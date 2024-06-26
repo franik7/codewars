8ee9503f49c20466afc10ea4d53975913c818fc0
Frants <118231497+franik7@users.noreply.github.com>
2024-06-26 18:51:09 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -3492,3 +3492,18 @@ function doubleInteger(i) {
   // i will be an integer. Double it and return it.
   return 2*i;
 }
+
+
+//Calculate BMI
+function bmi(weight, height) {
+  let bmi = weight / (Math.pow(height, 2))
+  if(bmi <= 18.5){
+    return "Underweight"
+  } else if(bmi <= 25){
+    return "Normal"
+  } else if(bmi <= 30){
+    return "Overweight"
+  } else {
+    return "Obese"
+  }
+}

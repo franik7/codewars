40f379859d3b3b1c8663debc2867ade72cfaa325
Frants <118231497+franik7@users.noreply.github.com>
2024-07-06 18:59:58 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -3630,3 +3630,14 @@ function peopleWithAgeDrink(old) {
       return "drink whisky"
     }
 };
+
+
+//You only need one - Beginner
+function check(a, x) {
+  // your code here
+  for(let i = 0; i<a.length;i++){
+    if(a[i] == x){
+      return true
+    } 
+  } return false
+}

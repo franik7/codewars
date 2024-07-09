f28a8c4122501ccfca1b100f2d4d21437553a3a8
Frants <118231497+franik7@users.noreply.github.com>
2024-07-09 14:37:43 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -3643,4 +3643,22 @@ function check(a, x) {
 }
 
 	
-
+//L1: Bartender, drinks!
+function getDrinkByProfession(param){
+  let paramLower = param.toLowerCase()
+  if(paramLower == "jabroni"){
+    return "Patron Tequila"
+  } else if(paramLower == "school counselor"){
+    return "Anything with Alcohol"
+  } else if(paramLower == "programmer"){
+    return "Hipster Craft Beer"
+  } else if(paramLower == "bike gang member"){
+    return "Moonshine"
+  } else if(paramLower == "politician"){
+    return "Your tax dollars"
+  } else if(paramLower == "rapper"){
+    return "Cristal"
+  } else {
+    return "Beer"
+  }
+}

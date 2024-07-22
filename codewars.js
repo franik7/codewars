23adb10cc2bb82aa5d778cb99413ee9c528dbf54
Frants <118231497+franik7@users.noreply.github.com>
2024-07-21 22:32:54 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -3777,3 +3777,18 @@ function noBoringZeros(n) {
   return Number(newStr);
 }
 
+	
+//What's the real floor?
+function getRealFloor(n) {
+  if(n<0){
+    return n
+  } else if(n == 0){
+    return 0
+  } else if(n == 13){
+    return 12
+  } else if(n<13) {
+    return n - 1
+  } else {
+    return n - 2
+  }
+}

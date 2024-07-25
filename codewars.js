4745b8744fe930106cb6951753fd3090d62f0fa7
Frants <118231497+franik7@users.noreply.github.com>
2024-07-25 15:34:39 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -3813,3 +3813,26 @@ function squareDigits(num){
   }
   return Number(concatArr.join(""))
 }
+
+
+//Return the day
+function whatday(num) { 
+  // put your code here
+  if(num == 1){
+    return "Sunday"
+  } else if (num == 2){
+    return "Monday"
+  } else if (num == 3){
+    return "Tuesday"
+  } else if (num == 4){
+    return "Wednesday"
+  } else if (num == 5){
+    return "Thursday"
+  } else if (num == 6){
+    return "Friday"
+  } else if (num == 7){
+    return "Saturday"
+  } else {
+    return "Wrong, please enter a number between 1 and 7"
+  }
+}

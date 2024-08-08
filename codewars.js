ec139769f6259330559faeb66b5067620e276c1d
Frants <118231497+franik7@users.noreply.github.com>
2024-08-08 12:28:27 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -3956,3 +3956,14 @@ Sleigh.prototype.authenticate = function(name, password) {
 };
 
 
+//Find the stray number
+function stray(numbers) {
+  for(let i = 0;i<numbers.length;i++){
+    if(numbers[i] != numbers[i+1] && numbers[i+1] == numbers[i+2]){
+      return numbers[i]
+    } else if(numbers[i] != numbers[i+1]){
+      return numbers[i+1]
+    }
+  }
+}
+

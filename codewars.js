ad635feed5272e30b87e8a12017e2958d03f5350
Frants <118231497+franik7@users.noreply.github.com>
2024-08-09 12:32:40 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -3967,3 +3967,21 @@ function stray(numbers) {
   }
 }
 
+	
+//Tip Calculator
+function calculateTip(amount, rating) {
+  rating = rating.toLowerCase()
+  if(rating == "terrible"){
+    return 0
+  } else if(rating == "poor"){
+    return Math.ceil(amount * 0.05)
+  } else if(rating == "good"){
+    return Math.ceil(amount * 0.1)
+  } else if(rating == "great"){
+    return  Math.ceil(amount * 0.15)
+  } else if(rating == "excellent"){
+    return  Math.ceil(amount * 0.2)
+  } else {
+    return "Rating not recognised"
+  }
+}

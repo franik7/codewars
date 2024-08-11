f0b9c293f7d2769d39f91ea6f4cb0317d05f86fa
Frants <118231497+franik7@users.noreply.github.com>
2024-08-10 20:47:50 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -3985,3 +3985,18 @@ function calculateTip(amount, rating) {
     return "Rating not recognised"
   }
 }
+
+	
+//L1: Set Alarm
+function setAlarm(employed, vacation){
+  if(employed == true && vacation == true){
+    return false
+  } else if(employed == true && vacation == false){
+    return true
+  } else if(employed == false && vacation == true){
+    return false
+  } else if(employed == false && vacation == false){
+    return false
+  }
+}
+

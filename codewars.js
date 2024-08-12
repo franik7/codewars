4302d42686e958e920979527f7f31d3197d98f72
Frants <118231497+franik7@users.noreply.github.com>
2024-08-12 17:48:12 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -4006,3 +4006,13 @@ function derive(coefficient,exponent) {
   let product = coefficient * exponent
   return `${product}x^${exponent - 1}`
 }
+
+
+//Fix your code before the garden dies!
+function rainAmount(mm){
+    if (mm < 40) {
+         return `You need to give your plant ${40 - mm}mm of water`
+    } else {
+         return "Your plant has had more than enough water for today!"
+    };
+}

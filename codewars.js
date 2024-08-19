7514e74f72e0a21784b234ba3edf00a990ac5270
Frants <118231497+franik7@users.noreply.github.com>
2024-08-19 18:08:38 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -4085,3 +4085,23 @@ function odds(values){
   // arrow it
   return values.filter(el => el % 2 != 0);
 }
+
+	
+//Maximum Length Difference
+function mxdiflg(a1, a2) {
+    // your code
+  if(a1.length == 0 || a2.length == 0){
+    return -1
+  }
+  a1.sort((a,b) => a.length - b.length)
+  a2.sort((a,b) => a.length - b.length)
+  
+  let firstScenario = a1[a1.length-1].length - a2[0].length
+  let secondScenario = a2[a2.length-1].length - a1[0].length
+  
+  if(firstScenario>secondScenario){
+    return firstScenario
+  } else {
+    return secondScenario
+  }
+}

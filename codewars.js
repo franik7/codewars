73fc6a2bc20333d492d598d7ab022eac9bd0c837
Frants <118231497+franik7@users.noreply.github.com>
2024-08-24 13:46:21 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -4152,3 +4152,17 @@ function toNumberArray(stringarray){
     numArray.push(Number(stringarray[i]))
   } return numArray
 }
+
+
+//Leonardo Dicaprio and Oscars
+function leo(oscar){
+  if(oscar == 88){
+    return "Leo finally won the oscar! Leo is happy"
+  } else if(oscar == 86){
+    return "Not even for Wolf of wallstreet?!"
+  } else if(oscar != 88 && oscar !=86 && oscar < 88){
+    return "When will you give Leo an Oscar?"
+  } else {
+    return "Leo got one already!"
+  }
+}

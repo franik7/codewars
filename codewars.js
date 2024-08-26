dfc704e0447df2681391e6468d36f9c11b0636a9
Frants <118231497+franik7@users.noreply.github.com>
2024-08-26 18:22:56 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -4166,3 +4166,14 @@ function leo(oscar){
     return "Leo got one already!"
   }
 }
+
+
+//Find the Remainder
+function remainder(n, m){
+  // Divide the larger argument by the smaller argument and return the remainder
+  if(n > m){
+    return n % m
+  } else {
+    return m % n
+  }
+}

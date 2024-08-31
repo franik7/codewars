38395321d2230462b6e2c96c3b6ede31c6445328
Frants <118231497+franik7@users.noreply.github.com>
2024-08-31 17:55:35 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -4223,3 +4223,45 @@ function solve(arr) {
   }
   return newArr3
 }
+
+
+//Welcome!
+function greet(language) {
+  if (language === "english") {
+    return "Welcome"
+  } else if (language === "czech") {
+    return "Vitejte"
+  } else if (language === "danish") {
+    return "Velkomst"
+  } else if (language === "dutch") {
+    return "Welkom"
+  } else if (language === "estonian") {
+    return "Tere tulemast";
+  } else if (language === "finnish") {
+    return "Tervetuloa"
+  } else if (language === "flemish") {
+    return "Welgekomen"
+  } else if (language === "french") {
+    return "Bienvenue"
+  } else if (language === "german") {
+    return "Willkommen"
+  } else if (language === "irish") {
+    return "Failte";
+  } else if (language === "italian") {
+    return "Benvenuto"
+  } else if (language === "latvian") {
+    return "Gaidits"
+  } else if (language === "lithuanian") {
+    return "Laukiamas"
+  } else if (language === "polish") {
+    return "Witamy"
+  } else if (language === "spanish") {
+    return "Bienvenido"
+  } else if (language === "swedish") {
+    return "Valkommen"
+  } else if (language === "welsh") {
+    return "Croeso"
+  } else {
+    return "Welcome"
+  }
+}

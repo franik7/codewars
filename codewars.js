3541bbcf93fcada1a58ae94d0b3bbd9ce379906a
Frants <118231497+franik7@users.noreply.github.com>
2024-09-16 16:50:08 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -4420,3 +4420,18 @@ function typeOfSum(a, b) {
   // good luck
   return typeof(a + b)
 }
+
+	
+//Define a card suit
+function defineSuit(card) {
+  // good luck
+  if(card[card.length-1] == "♣"){
+    return "clubs"
+  } else if (card[card.length-1] == "♦"){
+    return "diamonds"
+  } else if (card[card.length-1] == "♥"){
+    return "hearts"
+  } else {
+    return "spades"
+  }
+}

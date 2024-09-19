d14efdac3641d1c4be1cca7b6d6592bc26904cdb
Frants <118231497+franik7@users.noreply.github.com>
2024-09-19 17:20:34 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -4435,3 +4435,17 @@ function defineSuit(card) {
     return "spades"
   }
 }
+
+
+//Pythagorean Triple
+function isPythagoreanTriple(integers) {
+  if (Math.pow(integers[0], 2) + Math.pow(integers[1], 2) === Math.pow(integers[2], 2)) {
+    return true;
+  } else if (Math.pow(integers[1], 2) + Math.pow(integers[2], 2) === Math.pow(integers[0], 2)) {
+    return true;
+  } else if (Math.pow(integers[0], 2) + Math.pow(integers[2], 2) === Math.pow(integers[1], 2)) {
+    return true;
+  } else {
+    return false;
+  }
+}

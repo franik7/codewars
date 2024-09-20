06b49137a28191bf5f64dbd3196497b55208d6e5
Frants <118231497+franik7@users.noreply.github.com>
2024-09-20 16:31:01 -0400

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -4449,3 +4449,16 @@ function isPythagoreanTriple(integers) {
     return false;
   }
 }
+
+
+//Opposites Attract
+function lovefunc(flower1, flower2){
+  // moment of truth
+  if(flower1 % 2 == 0 && flower2 % 2 != 0){
+    return true
+  } else if (flower2 % 2 == 0 && flower1 % 2 != 0){
+    return true
+  } else {
+    return false
+  }
+}

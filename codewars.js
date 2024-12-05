d1fb08bca478041ee91aae2eaa6c0ff0bdf2d82c
Frants <118231497+franik7@users.noreply.github.com>
2024-12-05 09:34:42 -0500

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -5222,3 +5222,8 @@ function maxProduct(numbers, size){
   return product
 }
 
+
+//Basic Training: Add item to an Array
+// add the value "codewars" to the already defined websites array
+websites.push("codewars")
+

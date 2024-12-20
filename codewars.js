6bf73473f695a0e9553f991048c5c54146233e4e
Frants <118231497+franik7@users.noreply.github.com>
2024-12-20 11:46:08 -0500

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -5304,3 +5304,14 @@ function periodIsLate(last, today, cycleLength) {
     return false
   }
 }
+
+	
+//Power
+function numberToPower(number, power){
+	
+let product = 1;
+for(let i = 0;i<power;i++){
+  product *= number 
+}
+  return product
+}
